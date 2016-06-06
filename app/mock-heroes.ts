7434dc4f75164fcabbf80f29fa70c3da835424bb
John Breau <john.breau@vml.com>
2016-06-06 09:45:06 -0500

    folder structure cleanup

--- app/mock-heroes.ts
+++ app/mock-heroes.ts
@@ -9,5 +9,9 @@ export var HEROES: Hero[] = [
   {id: 17, name: 'Dynama'},
   {id: 18, name: 'Dr IQ'},
   {id: 19, name: 'Magma'},
-  {id: 20, name: 'Tornado'}
+  {id: 20, name: 'Tornado'},
+  {id: 21, name: 'Destructo'},
+  {id: 22, name: 'CrazyAss'},
+  {id: 23, name: 'The Dirge'},
+  {id: 24, name: 'Flotilla the Killa'}
 ];
